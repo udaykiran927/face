6d6ca8bbacb7fa49ea83007d80916a07ab4a8d30
Udaykiran <108007033+udaykiran927@users.noreply.github.com>
2023-02-04 19:49:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+
+from flask import Flask, render_template, Response
+import cv2
+import numpy as np
+import os
+import numpy as np
+from datetime import datetime
+app=Flask(__name__)
+camera=cv2.VideoCapture(0)
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+def gen_frames():
+    camera=cv2.VideoCapture(0)
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            rgb_small_frame = small_frame[:, :, ::-1]
+            
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+if __name__=='__main__':
+    app.run(debug=True)
