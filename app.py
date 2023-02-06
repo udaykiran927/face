dc5af7bf7a2ed2bdb9fcd93b642da1867653c5c2
Udaykiran <108007033+udaykiran927@users.noreply.github.com>
2023-02-07 00:46:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,7 @@ def gen_frames():
             
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
